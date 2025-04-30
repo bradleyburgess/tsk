b341c93bab4acc722ffd8a4606202075a25020ce
Bradley Burgess <bradleyburgess@gmail.com>
2025-04-30 10:10:54 +0200

    feat(domain): Add input validation

---
+++ Tsk.Domain/Validators/Input.cs
@@ -0,0 +1,31 @@
+using Tsk.Domain.Validators.Utils;
+
+namespace Tsk.Domain.Validators
+{
+    public static class Input
+    {
+        public static void ValidateTag(string input)
+        {
+            Strings.NoCommas(input);
+            Strings.NoLineBreaks(input);
+            Strings.MaxLength(input, 20);
+            Strings.NotWhiteSpace(input);
+        }
+
+        public static void ValidateLocation(string input)
+        {
+            Strings.NoCommas(input);
+            Strings.NoLineBreaks(input);
+            Strings.MaxLength(input, 20);
+            Strings.NotWhiteSpace(input);
+        }
+
+        public static void ValidateDescription(string input)
+        {
+            // Strings.NoCommas(input);
+            Strings.NoLineBreaks(input);
+            Strings.MaxLength(input, 50);
+            Strings.NotWhiteSpace(input);
+        }
+    }
+}
---
+++ Tsk.Domain/Validators/Utils/Strings.cs
@@ -0,0 +1,30 @@
+namespace Tsk.Domain.Validators.Utils
+{
+    public static class Strings
+    {
+        public static void NoCommas(string input)
+        {
+            if (input.Contains(","))
+                throw new ArgumentException("Cannot contain a comma", nameof(input));
+        }
+
+        public static void NotWhiteSpace(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                throw new ArgumentNullException("Cannot by null or white space", nameof(input));
+        }
+
+        public static void NoLineBreaks(string input)
+        {
+            string[] invalid = { "\r", "\n" };
+            if (invalid.Any(t => input.Contains(t)))
+                throw new ArgumentException("Cannot contain line breaks", nameof(input));
+        }
+
+        public static void MaxLength(string input, int maxLength = 50)
+        {
+            if (input.Length > maxLength)
+                throw new ArgumentException($"Cannot be longer than {maxLength}", nameof(input));
+        }
+    }
+}
