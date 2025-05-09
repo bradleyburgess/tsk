7f9392b975b2aa72ed4b724c43f78559bc04ed7a
Bradley Burgess <bradleyburgess@gmail.com>
2025-05-09 19:49:01 +0200

    refactor(infra,cli): Move EnsureTskPath to FlatFileRepositoryFactory

--- src/Tsk.CLI/Utils/Helpers.cs
+++ src/Tsk.CLI/Utils/Helpers.cs
@@ -4,62 +4,7 @@ namespace Tsk.CLI.Utils;
 
 public class Helpers
 {
-    /// <summary>
-    /// Expands a user-specified path, resolving to absolute path.
-    /// </summary>
-    /// <param name="path"></param>
-    /// <returns>The fully-qualified absolute path</returns>
-    /// <exception cref="ArgumentException"></exception>
-    public static string ExpandUserPath(string path)
-    {
-        if (string.IsNullOrWhiteSpace(path))
-            throw new ArgumentException("Path cannot be null or empty.", nameof(path));
 
-        if (path.StartsWith("~/") || path.StartsWith("~\\"))
-        {
-            var home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-            return Path.Combine(home, path[2..]); // strips "~/"
-        }
 
-        return Path.GetFullPath(path); // normalize relative paths
-    }
-
-    /// <summary>
-    /// Returns the user's home directory. Compatible with Windows, MacOS, and Linux.
-    /// </summary>
-    /// <returns>The user's home directory</returns>
-    public static string GetUserHomeDirectory() =>
-        OperatingSystem.IsWindows()
-        ? Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)
-        : OperatingSystem.IsMacOS()
-        ? Path.Combine(
-            Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
-            "Library",
-            "Application Support"
-        )
-        : Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-
-    /// <summary>
-    /// Ensures that the tsk file exists
-    /// </summary>
-    /// <param name="path"></param>
-    /// <exception cref="ArgumentNullException"></exception>
-    /// <exception cref="DirectoryNotFoundException"></exception>
-    /// <exception cref="UnauthorizedAccessException"></exception>
-    public static void EnsureTskFile(string path)
-    {
-        string? _path = path;
-        if (_path == AppData.GetDefaultTskPath())
-        {
-            Directory.CreateDirectory(Path.GetDirectoryName(_path)!);
-        }
-        string? parentDir = Path.GetDirectoryName(_path);
-        if (string.IsNullOrEmpty(parentDir))
-            throw new ArgumentNullException(parentDir);
-        if (!File.Exists(_path))
-        {
-            AnsiConsole.WriteLine($"Creating {path}.");
-            File.Create(_path!).Close();
-        }
-    }
+    
 }
--- src/Tsk.Infrastructure/Factories/FlatFileTodoRepositoryFactory.cs
+++ src/Tsk.Infrastructure/Factories/FlatFileTodoRepositoryFactory.cs
@@ -2,6 +2,7 @@ using Tsk.Domain.Factories;
 using Tsk.Domain.Repositories;
 using Tsk.Infrastructure.FileIO;
 using Tsk.Infrastructure.Repositories;
+using Tsk.Infrastructure.Utils;
 
 namespace Tsk.Infrastructure.Factories
 {
@@ -9,10 +10,12 @@ namespace Tsk.Infrastructure.Factories
     {
         public ITodoRepository Create(string path)
         {
+            Helpers.EnsureTskFile(path);
             var fileIO = new FileReaderWriter();
             var repo = new FlatFileTodoRepository(path, fileIO);
             repo.LoadTodos();
             return repo;
+
         }
     }
 }
